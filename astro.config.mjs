3f308b57eef6942ed42f08cafa8c0a7d719c5b61
Souleh Shaikh <soulehshaikh99@gmail.com>
2026-05-31 00:50:09 +0530

    Added shadcn/ui to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import { buildIntegration } from './astro.build.mjs';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   compressHTML: true,
   server: {
     port: 8000,
   },
-  integrations: [buildIntegration()],
+  integrations: [buildIntegration(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
